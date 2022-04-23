629e789ee1fa2a3b5407af519dfc2e28f4699701
evanm <emartin1455@protonmail.com>
2022-04-23 13:50:57 -0700

    Added 4th menu to ProfessionMenu "items", select up to 2 items from list. Needs a new DB field.
    Fixed ProfessionMenuView timeout after successful selection issue

--- professionMenuView.py
+++ professionMenuView.py
@@ -3,14 +3,14 @@ import db
 
 
 class ProfessionMenu(discord.ui.Select):
-    def __init__(self, parent_message, category, clas=None, units=None, first_entry=None):
+    def __init__(self, parent_message, category, clas=None, units=None, level=None, first_entry=None):
         self.parent_message = parent_message
         self.category = category
         self.clas = clas
         self.units = units
         self.first_entry = first_entry
+        self.level = level
         max_vals = 1
-        print(first_entry)
 
         # Set the options that will be presented inside the dropdown
         if category == "class":
@@ -48,13 +48,22 @@ class ProfessionMenu(discord.ui.Select):
                 print(f"ERROR: Dropdown optional parameter 'clas': {self.clas} invalid")
                 return
 
+        elif category == "items":
+            options = [
+                discord.SelectOption(label='None'),
+                discord.SelectOption(label='Item1'),
+                discord.SelectOption(label='Item2')
+            ]
+            max_vals = 2
+
         else:
             print("ERROR: Dropdown required parameter 'category' either empty or invalid")
             return
 
         placeholder = f'Choose your {category}'
-        if category == 'unit':
-            placeholder += ' (PICK UP TO 3)'
+        if max_vals > 1:
+            placeholder += f' (PICK UP TO {max_vals})'
+
         placeholder += '...'
         super().__init__(placeholder=placeholder, min_values=1, max_values=max_vals, options=options)
 
@@ -76,7 +85,10 @@ class ProfessionMenu(discord.ui.Select):
         elif self.category == "unit":
             self.units = choice
             nextCategory = "level"
-        else:
+        elif self.category == "level":
+            self.level = choice
+            nextCategory = "items"
+        else:   # category is items
             nextCategory = None
 
         if nextCategory is not None:
@@ -84,13 +96,16 @@ class ProfessionMenu(discord.ui.Select):
             await interaction.response.edit_message(
                 content=f'You chose: {choice}',
                 view=ProfessionMenuView(self.parent_message, nextCategory,
-                                        clas=self.clas, units=self.units, first_entry=self.first_entry)
+                                        clas=self.clas, units=self.units, level=self.level,
+                                        first_entry=self.first_entry)
             )
-        else:   # the current category is "level"
+        else:   # the current category is "item"
             # Remove the selectmenu, tell user what they selected, write the info to DB
 
-            level = self.values[0]
-            formattedProfString = f'CLASS: **{self.clas}**, UNIT(s): **{self.units}**, LEVEL: **{level}**'
+            formattedProfString = f'**CLASS:** {self.clas}, **UNIT(s)**: {self.units}, **LEVEL**: {self.level}'
+            if 'None' not in self.values:
+                items = ', '.join(self.values)
+                formattedProfString += f', **ITEM(s):** {items}'
 
             # parse selection info into appropriate form for DB-submission
             unitCharDict = {'Army': 'A', 'Air Force': 'F', 'Navy': 'N'}
@@ -98,9 +113,9 @@ class ProfessionMenu(discord.ui.Select):
 
             ceLevelDict = {"2": 0, "3": 1, "3X": 2, "3XE": 3}
             mmLevelDict = {"0T": 0, "3T": 1, "5T": 2, "10": 3, "E": 4}
-            level = ceLevelDict[level] if self.clas == 'CE' else mmLevelDict[level]
+            levelNum = ceLevelDict[self.level] if self.clas == 'CE' else mmLevelDict[self.level]
 
-            prof_array = [self.clas, ''.join(unitChars), level]
+            prof_array = [self.clas, ''.join(unitChars), levelNum]
 
             # if first-time user does not have an entry in DB (called through dm.request_entry())
             if self.first_entry:
@@ -114,19 +129,27 @@ class ProfessionMenu(discord.ui.Select):
                 msg = 'Your profession has been updated to:\n'
 
             # send user's selection, remove the view
+
             msg += formattedProfString
             await interaction.response.edit_message(content=msg, view=None)
 
 
 class ProfessionMenuView(discord.ui.View):
-    def __init__(self, parent_message, category, clas=None, units=None, first_entry=None):
+    def __init__(self, parent_message, category, clas=None, units=None, level=None, first_entry=None):
         super().__init__(timeout=300)
         self.parent_message = parent_message
         self.first_entry = first_entry
 
         # Adds the dropdown to our view object.
-        self.add_item(ProfessionMenu(parent_message, category, clas=clas, units=units, first_entry=first_entry))
+        self.add_item(ProfessionMenu(parent_message, category, clas=clas, units=units, level=level, first_entry=first_entry))
 
     async def on_timeout(self):
-        print('timeout!')
-        await self.parent_message.edit(content='Profession menu timed out.', view=None)
+        dm_channel = self.parent_message.channel
+        ID = self.parent_message.id
+        parent_message = await dm_channel.fetch_message(ID)
+        current_content = parent_message.content
+
+        # Check that the message has been edited to the selection completion strings, indicating that the user
+        # successfully completed selection. If not, timeout the message.
+        if not (current_content.startswith('You are') or current_content.startswith('Your profession')):
+            await self.parent_message.edit(content='Profession menu timed out.', view=None)
